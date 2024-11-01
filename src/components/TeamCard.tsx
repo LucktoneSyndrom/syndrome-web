f37dbd24d495967aeca5bb7ddb52eab651b966f9
yuri295 <jin_astro0302@naver.com>
2024-11-01 20:11:41 +0900

    feat(team-card): 팀카드 디자인 수정

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import { Chip, Stack } from "@mui/material";
+import { Chip, Stack, Divider } from "@mui/material";
 import styles from "./TeamCard.module.css";
 import { TeamCardInfo } from "../types/TeamCardInfo";
 
@@ -24,54 +24,60 @@ const TeamCard: React.FC<TeamCardProps> = ({ teamCardInfo, isSelected }) => {
   };
 
   return (
-    <div className={styles.teamCard} onClick={handleDetailsClick}>
-      <h3 className={styles.shortDescription}>
-        {teamCardInfo.shortDescription}
-      </h3>
-      <Stack direction="row" spacing={0.5}>
-        {teamCardInfo.collectPart.map((part, index) => (
-          <Chip
-            key={index}
-            label={part}
-            className={styles.chip}
-            color={
-              part === "Frontend"
-                ? "error"
-                : part === "Backend"
-                ? "primary"
-                : part === "Mobile"
-                ? "success"
-                : "default"
-            }
-          />
-        ))}
-      </Stack>
+    <Stack>
+      <div className={styles.teamCard} onClick={handleDetailsClick}>
+        <p className={styles.teamName}>{teamCardInfo.teamName}</p>
+        <h3 className={styles.shortDescription}>
+          {teamCardInfo.shortDescription}
+        </h3>
+        <Stack direction="row" spacing={0.5}>
+          {teamCardInfo.collectPart.map((part, index) => (
+            <Chip
+              key={index}
+              label={part}
+              className={styles.chip}
+              color={
+                part === "Frontend"
+                  ? "error"
+                  : part === "Backend"
+                  ? "primary"
+                  : part === "Mobile"
+                  ? "success"
+                  : "default"
+              }
+            />
+          ))}
+        </Stack>
+        <div className={styles.infoContainer}>
+          <div className={styles.recruitmentParts}>
+            <span className={styles.recruitmentPartLabel}>모집 인원: </span>
+            {teamCardInfo.recruitmentParts.map((part, index) => (
+              <span className={styles.recruitmentPart} key={index}>
+                {part.partName} {part.numberNeeded}명
+                {index < teamCardInfo.recruitmentParts.length - 1 && ", "}
+              </span>
+            ))}
+          </div>
+          <Divider orientation="vertical" sx={{ height: "10px" }} />
+          <div className={styles.deadlineContainer}>
+            <span className={styles.deadline}>
+              모집 기간: {teamCardInfo.deadline}
+            </span>
+            <span className={styles.deadlineDay}>
+              D-{teamCardInfo.deadlineDay}
+            </span>
+          </div>
+          <Divider orientation="vertical" sx={{ height: "10px" }} />
+          <p className={styles.tag}># {teamCardInfo.tag}</p>
+        </div>
 
-      <p className={styles.teamName}>{teamCardInfo.teamName}</p>
-
-      <div className={styles.recruitmentParts}>
-        <span className={styles.recruitmentPartLabel}>모집 인원: </span>
-        {teamCardInfo.recruitmentParts.map((part, index) => (
-          <span className={styles.recruitmentPart} key={index}>
-            {part.partName} {part.numberNeeded}명
-            {index < teamCardInfo.recruitmentParts.length - 1 && ", "}
-          </span>
-        ))}
+        {isSelected && (
+          <button className={styles.chatButton} onClick={handleChatClick}>
+            팀 채팅
+          </button>
+        )}
       </div>
-
-      <div className={styles.deadlineContainer}>
-        <span className={styles.deadline}>
-          모집 기간: {teamCardInfo.deadline}
-        </span>
-        <span className={styles.deadlineDay}>D-{teamCardInfo.deadlineDay}</span>
-      </div>
-      <p className={styles.tag}># {teamCardInfo.tag}</p>
-      {isSelected && (
-        <button className={styles.chatButton} onClick={handleChatClick}>
-          팀 채팅
-        </button>
-      )}
-    </div>
+    </Stack>
   );
 };
 
